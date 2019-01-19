2b02c77a200107c02385c1d71f2c833dbd5c1196
Henrique Repulho <henrique.repulho@gmail.com>
2019-01-19 15:17:53 -0200

    Test Code

---
+++ ajax_call_json.js
@@ -0,0 +1,22 @@
+
+    
+    // Simple way to get a call with json response
+    // Necessary import jquery-3.3.1.min.js
+
+    $.ajax({
+		url : "http://echo.jsontest.com/nome/henrique/sobrenome/lages",
+		method : "GET",
+		
+	}).done(function(data){        
+        
+        var ajax_call_data = data;
+        console.log(ajax_call_data);
+        
+        var name = ajax_call_data.nome;  
+        
+        var midlename = ajax_call_data.sobrenome;
+
+        console.log(name + " " + midlename);          
+               
+	});
+
